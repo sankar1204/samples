d8c16b9a94782f9c7f78ed29943962d8a454f9f1
S <shankara.swamy@philips.com>
2020-10-11 22:24:49 +0530

    last commit of the day

--- IEnumerableExperiment/MyProgram.cs
+++ IEnumerableExperiment/MyProgram.cs
@@ -1,32 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading;
 
 namespace IEnumerableExperiment {
-    public class MyProgram {
+    abstract public class MyProgram {
 
-        public static void Main(String[] args) {
+        internal delegate IEnumerable<string> MyDelegate(List<string> a);
+
+        public static void Main(String[] args)
+        {
+            MyDelegate a = MyPredicate;
+
             MyEnumerable ojb = new MyEnumerable();
 
             Func<List<string>, IEnumerable<string>> pred = (col) => { return col.Where(s => s.Length < 3); };
-
-            ojb.myCollection.AddRange(new[]{"UttaraKhand", "ladhak", "Kashmir", "Punjab", "Haryana", "goa", "12"});
-            foreach (var s in ojb.MySelect(MyPredicate)) {
+            ojb.myCollection.AddRange(new[] { "UttaraKhand", "ladhak", "Kashmir", "Punjab", "Haryana", "goa", "12" });
+            foreach (var s in ojb.MySelect(pred)) {
                 Console.WriteLine(s);
             }
             Console.WriteLine("Hello to Myworld");
             Console.ReadLine();
         }
 
-        internal IEnumerable<string> MyPredicate(List<string> col)
-        {
+        internal IEnumerable<string> MyPredicate(List<string> col) {
             return col.Where(s => s.Length < 3);
         }
 
         internal void LoopThrough() {
-
 
         }
     }
