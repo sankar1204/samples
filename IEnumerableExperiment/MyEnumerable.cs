4c88a1f9932463b20216fcc1b9d14c2cc19cb97a
S <shankara.swamy@philips.com>
2020-10-11 21:43:38 +0530

    first peoject files

---
+++ IEnumerableExperiment/MyEnumerable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IEnumerableExperiment {
+    public class MyEnumerable {
+
+
+        internal List<string> myCollection = new List<string>();
+
+        internal IEnumerable<string> MySelect(Func<List<string>, IEnumerable<string>> pred) {
+            return pred(myCollection);
+        }
+    }
+}
---
+++ IEnumerableExperiment/MyProgram.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace IEnumerableExperiment {
+    public class MyProgram {
+
+        public static void Main(String[] args) {
+            MyEnumerable ojb = new MyEnumerable();
+
+            Func<List<string>, IEnumerable<string>> pred = (col) => { return col.Where(s => s.Length < 3); };
+
+            ojb.myCollection.AddRange(new[]{"UttaraKhand", "ladhak", "Kashmir", "Punjab", "Haryana", "goa", "12"});
+            foreach (var s in ojb.MySelect(MyPredicate)) {
+                Console.WriteLine(s);
+            }
+            Console.WriteLine("Hello to Myworld");
+            Console.ReadLine();
+        }
+
+        internal IEnumerable<string> MyPredicate(List<string> col)
+        {
+            return col.Where(s => s.Length < 3);
+        }
+
+        internal void LoopThrough() {
+
+
+        }
+    }
+}
