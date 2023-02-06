342232be5ad68890b9b7e5a36db1065c489c66c2
310227436 <shankaraswamy.s@philips.com>
2023-02-06 10:34:52 +0530

    SQS-Lambda batch message recieving and partial sucess or error handling

---
+++ SqsLambda/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaTest
+{
+    internal class Order
+    {
+        public string Id { get; set; }
+        public DateTime OrderTime { get; set; }
+        public string OrderName { get; set; }
+        public long OrderValue { get; set; }
+        public string OrderDescription { get; set; }
+    }
+}
